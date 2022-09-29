6ee663f6163ff2006c113e6801915d9f1be7e086
CiroWen <cirowencs@gmail.com>
2022-09-28 17:35:28 -0700

    added html and js for medium questions, complete rotate Array with 2 approach(1 doesn't meet the requirement but workable)

--- easy.js
+++ easy.js
@@ -69,22 +69,71 @@ var twoSum = function (nums, target) {
  */
 //finds the element that is different from the previous one, then starting
 var removeDuplicates = function (nums) {
-  let count = 1 //counter and tracker
+  let count = 1; //counter and tracker
   for (let i = 0; i < nums.length - 1; i++) {
     //if the current element is different from the next one, we set the array at [count] the value of next element.
     // in the meanwhile, imcrement it.
-    if (nums[i] !== nums[i+1]){
-      nums[count++] = nums[i+1]
+    if (nums[i] !== nums[i + 1]) {
+      nums[count++] = nums[i + 1];
     }
-  
+
     console.log(nums);
     // i--
     // nums.push('_')
-
   }
   console.log(nums);
-  return count
+  return count;
 };
 
 // console.log(removeDuplicates([1, 1, 3, 3, 4, 5])); //passed
 // console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4])); //passed
+
+// 4. Rotate Array
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+//
+var rotate = function (nums, k) {
+  if (k === nums.length) return;
+  k = k % nums.length;
+
+  reverse(0, nums.length - 1); //reverse the whole array
+  reverse(0, k - 1); //reverse the first half of the array
+  reverse(k, nums.length - 1); //reverse the rest half of the array
+  function reverse(start, end) {
+    while (start < end) {
+      let temp = nums[start];
+      nums[start] = nums[end];
+      nums[end] = temp;
+      start++;
+      end--;
+    }
+  }
+  console.log(nums);
+};
+
+rotate([1, 2, 3, 4, 5, 6], 1);
+
+/**
+ * passes maximum time limit
+ */
+// var rotate = function (nums, k) {
+// if(k === nums.length) return
+// if(k > nums.length) k = k % nums.length
+//    while(k>0){
+//     // nums.shift(nums.push()) rotate to the left
+//     nums.unshift(nums.pop()) // rotate to the right
+//     k--
+//    }
+//   console.log(nums);
+// };
+
+// rotate([1, 2, 3, 4, 5, 6], 5);
+
+/**
+ * helperArr = [1,2,3,4]
+ * nums = [1,2,3,4]
+ * i = 0 nums = [4,1,]
+ *  */
---
+++ medium.js
@@ -0,0 +1,51 @@
+
+// 4. Rotate Array
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+//
+var rotate = function (nums, k) {
+    if (k === nums.length) return;
+    k = k % nums.length;
+  
+    reverse(0, nums.length - 1); //reverse the whole array
+    reverse(0, k - 1); //reverse the first half of the array
+    reverse(k, nums.length - 1); //reverse the rest half of the array
+    function reverse(start, end) {
+      while (start < end) {
+        let temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+      }s
+    }
+    console.log(nums);
+  };
+  
+  rotate([1, 2, 3, 4, 5, 6], 1);
+  
+  /**
+   * passes maximum time limit
+   */
+  // var rotate = function (nums, k) {
+  // if(k === nums.length) return
+  // if(k > nums.length) k = k % nums.length
+  //    while(k>0){
+  //     // nums.shift(nums.push()) rotate to the left
+  //     nums.unshift(nums.pop()) // rotate to the right
+  //     k--
+  //    }
+  //   console.log(nums);
+  // };
+  
+  // rotate([1, 2, 3, 4, 5, 6], 5);
+  
+  /**
+   * helperArr = [1,2,3,4]
+   * nums = [1,2,3,4]
+   * i = 0 nums = [4,1,]
+   *  */
+  
